f2cbc0f0e39faf28195392cac652d9b652f621de
maxinhang <maxinhangdoit@163.com>
2023-03-02 15:14:27 +0800

    TaskPool 加锁

--- src/task/TaskPool.java
+++ src/task/TaskPool.java
@@ -11,30 +11,45 @@ import java.util.concurrent.Executors;
  */
 public class TaskPool {
 
-    PriorityQueue<OptDelayScheduleTask> priorityQueue = new PriorityQueue<>(Comparator.comparing(OptDelayScheduleTask::getNextExecuteTime));
+    final PriorityQueue<OptDelayScheduleTask> priorityQueue = new PriorityQueue<>(Comparator.comparing(OptDelayScheduleTask::getNextExecuteTime));
     //可缓存的线程池，若线程数超过处理所需，缓存一段时间后会回收，若线程数不够，则新建线程。
     ExecutorService threadPool = Executors.newCachedThreadPool();
+
     public TaskPool() {
         //启动线程去执行任务
         Runnable runnable = () -> {
             while (true) {
-                if (!priorityQueue.isEmpty()) {
-                    OptDelayScheduleTask minTask = priorityQueue.peek();
-                    long nextExecuteTime = minTask.getNextExecuteTime();
-                    if (nextExecuteTime > System.currentTimeMillis()) {
-                        continue;
-                    }
-                    priorityQueue.poll();
-                    threadPool.submit(minTask.getRunnable());
-                    long newExecuteTime = minTask.getPried() + System.currentTimeMillis();
-                    minTask.setNextExecuteTime(newExecuteTime);
-                    priorityQueue.add(minTask);
-                }
+                loopExecute();
             }
         };
         new Thread(runnable).start();
     }
+
+    private void loopExecute() {
+        synchronized (priorityQueue) {
+            try {
+                while (priorityQueue.isEmpty()) {
+                    priorityQueue.wait();
+                }
+                OptDelayScheduleTask minTask = priorityQueue.peek();
+                long nextExecuteTime = minTask.getNextExecuteTime();
+                if (nextExecuteTime > System.currentTimeMillis()) {
+                    return;
+                }
+                priorityQueue.poll();
+                threadPool.submit(minTask.getRunnable());
+                long newExecuteTime = minTask.getPried() + System.currentTimeMillis();
+                minTask.setNextExecuteTime(newExecuteTime);
+                priorityQueue.add(minTask);
+            } catch (InterruptedException ignore) {
+            }
+        }
+    }
+
     public void submit(OptDelayScheduleTask task) {
-        priorityQueue.add(task);
+        synchronized (priorityQueue) {
+            priorityQueue.add(task);
+            priorityQueue.notify();
+        }
     }
 }
