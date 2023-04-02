0501e75a6cc1869aeabd78f9b12f088b9b1cbedf
maxinhang <maxinhangdoit@163.com>
2023-04-02 23:10:23 +0800

    时间轮代码

---
+++ src/task/timingwheel/TimerTask.java
@@ -0,0 +1,55 @@
+package task.timingwheel;
+
+public class TimerTask implements Runnable {
+
+    /**
+     * 延时时间
+     */
+    private long delayMs;
+    /**
+     * 任务所在的entry
+     */
+    private TimerTaskEntry timerTaskEntry;
+
+    private String desc;
+
+    public TimerTask(String desc, long delayMs) {
+        this.desc = desc;
+        this.delayMs = delayMs;
+        this.timerTaskEntry = null;
+    }
+
+    public TimerTaskEntry getTimerTaskEntry() {
+        return timerTaskEntry;
+    }
+
+    public long getDelayMs() {
+        return delayMs;
+    }
+
+    public void setDelayMs(long delayMs) {
+        this.delayMs = delayMs;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public synchronized void setTimerTaskEntry(TimerTaskEntry entry) {
+        // 如果这个TimerTask已经被一个已存在的TimerTaskEntry持有,先移除一个
+        if (timerTaskEntry != null && timerTaskEntry != entry) {
+            timerTaskEntry.remove();
+        }
+        timerTaskEntry = entry;
+    }
+
+    @Override
+    public void run() {
+//        log.info("============={}任务执行", desc);
+    }
+
+}
