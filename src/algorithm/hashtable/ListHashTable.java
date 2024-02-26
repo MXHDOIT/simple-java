9acaa0b4e3bd046d223f7780784f6ddac2e271c0
Xinhang Ma <maxinhangdoit@163.com>
2024-02-26 17:19:52 +0800

    Hash table

---
+++ src/algorithm/hashtable/ListHashTable.java
@@ -0,0 +1,11 @@
+package algorithm.hashtable;
+
+/**
+ * ● 实现一个基于链表法解决冲突问题的散列表
+ *
+ * @author: maxinhang.
+ * @version: 2024/2/26 17:14.
+ */
+public class ListHashTable {
+
+}
---
+++ src/algorithm/hashtable/LruCache.java
@@ -0,0 +1,10 @@
+package algorithm.hashtable;
+
+/**
+ *  ● 实现一个LRU缓存淘汰算法
+ *
+ * @author: maxinhang.
+ * @version: 2024/2/26 17:14.
+ */
+public class LruCache {
+}
