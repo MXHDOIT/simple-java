26b98429acbb80017339d0318b39c99931908e09
Xinhang Ma <maxinhangdoit@163.com>
2024-03-05 09:35:43 +0800

    20240304.正则表达式匹配-50%

--- src/exercise/Solution.java
+++ src/exercise/Solution.java
@@ -44,7 +44,24 @@ public class Solution {
      * '*' 匹配零个或多个前面的那一个元素
      */
     public boolean isMatch(String s, String p) {
-        return false;
+        int sLength = s.length() + 1;
+        int pLength = p.length() + 1;
+        boolean[][] dp = new boolean[sLength][pLength];
+        dp[0][0] = true;
+        for (int i = 2; i < pLength; i++) {
+            dp[0][i] = dp[0][i-2] && p.charAt(i-1) == '*';
+        }
+
+        for (int i = 1; i < sLength; i++) {
+            for (int j = 1; j < pLength; j++) {
+                if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.'));
+                }else {
+                    dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.');
+                }
+            }
+        }
+        return dp[sLength-1][pLength-1];
     }
 
     /**
