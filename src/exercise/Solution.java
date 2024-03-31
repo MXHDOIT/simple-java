1d9f1f356412ba30dd648d9de96b3838f64c4782
Xinhang Ma <maxinhangdoit@163.com>
2024-03-31 22:15:08 +0800

    20240331.最长有效括号

--- src/exercise/Solution.java
+++ src/exercise/Solution.java
@@ -102,6 +102,28 @@ public class Solution {
     }
 
     /**
+     * 最长有效括号.
+     * dp[i] 表示以下标 i 字符结尾的最长有效括号的长度.
+     */
+    public int longestValidParentheses(String s) {
+        int length = s.length();
+        int[] dp = new int[length];
+        int maxans = Integer.MIN_VALUE;
+        for (int i = 1; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
+    /**
      * 下一个排列.
      */
     public void nextPermutation(int[] nums) {
