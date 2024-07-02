11b878c4191b24fc72679a1ada37963ec8fcb976
Xinhang Ma <maxinhangdoit@163.com>
2024-07-02 15:49:20 +0800

    20240702.缺失的第一个正数

--- src/exercise/Solution.java
+++ src/exercise/Solution.java
@@ -129,6 +129,22 @@ public class Solution {
         System.out.println(combinationSum2);
     }
 
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
     public int maximumPrimeDifference(int[] nums) {
         int beginIndex = 0;
         for (int i = 0; i < nums.length; i++) {
